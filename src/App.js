b973b493b76e08f18be34f82e12aae3c682d25b0
Daniil <daniilrealgrad@gmail.com>
2020-04-11 12:05:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Navigation from './common/Navbar'
+import Home from './Components/Home';
+import About from './Components/About';
+import Contacts from './Components/Contacts';
+import NoMatch from './Components/NoMatch';
+import Layout from './common/Layout'
+import Jumbotron from './common/Jumbotron'
 
-function App() {
+const App = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navigation />
+      <Jumbotron />
+      <Layout>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/about' component={About}/>
+          <Route exact path='/contacts' component={Contacts}/>
+          <Route component={NoMatch}/>
+        </Switch>
+      </BrowserRouter>
+      </Layout>
+    </React.Fragment>
   );
 }
 
