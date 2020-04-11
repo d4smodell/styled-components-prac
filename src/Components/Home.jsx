d61c816502ae789d453f5bea4c96fad1a3aae47b
Daniil <daniilrealgrad@gmail.com>
2020-04-11 12:05:19 +0300

    Create Home.jsx

---
+++ src/Components/Home.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Home = (props) => {
+  return (
+    <div>
+      <h2>Alice</h2>
+      <p>
+        Alice's Adventures in Wonderland was published in 1865, three years
+        after Charles Lutwidge Dodgson and the Reverend Robinson Duckworth, on 4
+        July, rowed up the Isis river (UK) in a boat with three young girls—a
+        day known as the "golden afternoon"
+      </p>
+      <p>
+        He would begin writing the manuscript of the story the next day,
+        although that earliest version is lost to history. The girls and Dodgson
+        took another boat trip a month later when he elaborated the plot to the
+        story of Alice, and in November he began working on the manuscript in
+        earnest.
+      </p>
+      <p>
+        Before Alice received her copy, Dodgson was already preparing it for
+        publication and expanding the 15,500-word original to 27,500 words,
+        most notably adding the episodes about the Cheshire Cat and the Mad Tea
+        Party.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
