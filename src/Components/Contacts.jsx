f5dbbc508a7546aeb81bc7b19ff54d4e3f313c17
Daniil <daniilrealgrad@gmail.com>
2020-04-11 12:05:16 +0300

    Create Contacts.jsx

---
+++ src/Components/Contacts.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Contacts = (props) => {
+  return (
+    <div>
+      <h2>Inspired by</h2>
+      <p>
+        Chapter Twelve – Alice's Evidence: Alice is then called up as a witness.
+        She accidentally knocks over the jury box with the animals inside them
+        and the King orders the animals be placed back into their seats before
+        the trial continues. The King and Queen order Alice to be gone, citing
+        Rule 42 ("All persons more than a mile high to leave the court"), but
+        Alice disputes their judgement and refuses to leave. She argues with the
+        King and Queen of Hearts over the ridiculous proceedings, eventually
+        refusing to hold her tongue, only to say, "It's not that I was the one
+        who stole the tarts in the first place", in the process. Finally, the
+        Queen confirms that Alice was the culprit responsible of stealing the
+        tarts after all (which automatically pardons the Knave of Hearts of his
+        charges), and shouts, "Off with her head!", but Alice is unafraid,
+        calling them just a pack of cards; although Alice holds her own for a
+        time, the card guards soon gang up and start to swarm all over her.
+        Alice's sister wakes her up from a dream, brushing what turns out to be
+        some leaves and not a shower of playing cards from Alice's face. Alice
+        leaves her sister on the bank to imagine all the curious happenings for
+        herself.
+      </p>
+      <p>
+        Chapter Ten – Lobster Quadrille: The Mock Turtle and the Gryphon dance
+        to the Lobster Quadrille, while Alice recites (rather incorrectly) "'Tis
+        the Voice of the Lobster". The Mock Turtle sings them "Beautiful Soup"
+        during which the Gryphon drags Alice away for an impending trial.
+      </p>
+      <p>
+        Chapter Seven – A Mad Tea-Party: Alice becomes a guest at a "mad" tea
+        party along with the March Hare, the Hatter, and a very tired Dormouse,
+        who falls asleep frequently only to be violently awakened moments later
+        by the March Hare and the Hatter. The characters give Alice many riddles
+        and stories, including the famous "why is a raven like a writing desk?."
+        The Hatter reveals that they have tea all day because Time has punished
+        him by eternally standing still at 6 PM (tea time). Alice becomes
+        insulted and tired of being bombarded with riddles and she leaves
+        claiming that it was the stupidest tea party that she had ever been to.
+      </p>
+    </div>
+  );
+};
+
+export default Contacts;
